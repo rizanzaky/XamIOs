481555c833068f019a4f4da74daa8a15dbbc723a
Rizan Zaky <rizan149@gmail.com>
2018-07-05 12:01:33 +0530

    initialize the view

---
+++ Gestures.IOs/ViewControllers/HomeViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Blank.Views.Home;
+using UIKit;
+
+namespace Blank.ViewControllers
+{
+    public class HomeViewController : UIViewController
+    {
+        private HomeView _homeView;
+
+        public HomeViewController()
+        {
+            _homeView = new HomeView();
+        }
+
+        public override void LoadView()
+        {
+            View = _homeView;
+        }
+
+        public override void ViewDidLoad()
+        {
+            WireUpDragGestureRecognizer();
+        }
+
+        private void WireUpDragGestureRecognizer()
+        {
+            var gesture = new UIPanGestureRecognizer();
+            gesture.AddTarget(() => HandleDrag(gesture));
+            _homeView.AddGestureRecognizer(gesture);
+        }
+
+        private void HandleDrag(UIPanGestureRecognizer gesture)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Gestures.IOs/Views/Home/HomeView.cs
@@ -0,0 +1,62 @@
+﻿using UIKit;
+
+namespace Blank.Views.Home
+{
+    public class HomeView : UIView
+    {
+        private UIView _topView;
+        private UIView _bottomView;
+        private NSLayoutConstraint _topConstraint;
+
+        public HomeView()
+        {
+            InitElements();
+        }
+
+        private void InitElements()
+        {
+            CreateElements();
+            SetStyles();
+            SetHierarchy();
+            SetConstraints();
+            BindData(); // TODO: MVVM
+        }
+
+        private void CreateElements()
+        {
+            _topView = new UIView { TranslatesAutoresizingMaskIntoConstraints = false };
+            _bottomView = new UIView { TranslatesAutoresizingMaskIntoConstraints = false };
+        }
+
+        private void SetStyles()
+        {
+            _topView.BackgroundColor = UIColor.Red;
+            _bottomView.BackgroundColor = UIColor.Blue;
+        }
+
+        private void SetHierarchy()
+        {
+            Add(_topView);
+            Add(_bottomView);
+        }
+
+        private void SetConstraints()
+        {
+            _topConstraint = _topView.TopAnchor.ConstraintEqualTo(TopAnchor);
+            _topConstraint.Active = true;
+            _topView.LeftAnchor.ConstraintEqualTo(LeftAnchor).Active = true;
+            _topView.RightAnchor.ConstraintEqualTo(RightAnchor).Active = true;
+            _topView.HeightAnchor.ConstraintEqualTo(80f).Active = true;
+
+            _bottomView.TopAnchor.ConstraintEqualTo(_topView.BottomAnchor).Active = true;
+            _bottomView.LeftAnchor.ConstraintEqualTo(LeftAnchor).Active = true;
+            _bottomView.RightAnchor.ConstraintEqualTo(RightAnchor).Active = true;
+            _bottomView.HeightAnchor.ConstraintEqualTo(1000f).Active = true;
+        }
+
+        private void BindData()
+        {
+
+        }
+    }
+}
